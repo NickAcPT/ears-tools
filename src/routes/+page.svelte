e621000dce96dcabdce9996af7c90e694ec0c91e
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-10-30 10:38:11 +0000

    Start basic homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<h1>Welcome to Ears Tools</h1>
+<p>Created by NickAcPT with 💖</p>
+
+<h2>Tools</h2>
+<div><a href="/tools/eraser">Region Eraser</a></div>
