e6210009bbd19edacd9c315ab4e7ee224926a047
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-10-30 23:35:23 +0000

    Navbar fix

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -4,7 +4,7 @@
 
 <nav class="py-4 border-text/20 border-b top-0 left-0 sticky backdrop-blur-2xl backdrop-saturate-150">
     <div class="{$$restProps.class}">
-        <a class="flex gap-2 items-center" href="{base}/">
+        <a class="flex gap-2 items-center max-w-fit" href="{base}/">
             <img class="h-max" src="{assets}/favicon.png" alt="NickAc's Minecraft skin face" />
             <h1 class="text-2xl">Ears Tools</h1>
         </a>
