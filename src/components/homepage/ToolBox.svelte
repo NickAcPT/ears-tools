e621000fd0bc798ae0ef9c8be34d77acc5d1bd96
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-10-30 23:55:56 +0000

    Yes

--- src/components/homepage/ToolBox.svelte
+++ src/components/homepage/ToolBox.svelte
@@ -9,9 +9,9 @@
 
 <div
     class:ghost
-    class="shadow-md h-full min-h-[10em] p-4 rounded-lg bg-primary-500 flex flex-col text-background hover:bg-primary-800 hover:shadow-lg"
+    class="shadow-md justify-around items-center h-full min-h-[10em] p-4 rounded-lg bg-primary-500 flex flex-col text-background hover:bg-primary-800 hover:shadow-lg"
 >
-    <div class="flex flex-col items-center gap-2">
+    <div class="flex items-center gap-2">
         <svelte:component this={icon} class="h-10" />
         <h2 class="text-left text-xl font-medium pb-1">{name}</h2>
     </div>
