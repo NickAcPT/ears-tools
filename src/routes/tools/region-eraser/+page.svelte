e6210006733745971b313745dd31eba1179b0dbc
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-11-01 10:37:29 +0000

    Select by drag

--- src/routes/tools/region-eraser/+page.svelte
+++ src/routes/tools/region-eraser/+page.svelte
@@ -56,10 +56,10 @@
 
         width = Math.max(1, width);
         height = Math.max(1, height);
-        
+
         width = Math.min(32, width);
         height = Math.min(32, height);
-        
+
         e.setSize([width * getImagePixelWidth(), height * getImagePixelHeight()]);
 
         e.target.setAttribute("data-width", width.toString());
@@ -182,6 +182,15 @@
     function addRegion(x: number, y: number, w: number, h: number) {
         if (!$workspace) return;
 
+        x = Math.max(0, x);
+        y = Math.max(0, y);
+
+        w = Math.max(1, w);
+        h = Math.max(1, h);
+
+        w = Math.min(32, w);
+        h = Math.min(32, h);
+
         $regions = [...$regions, { x, y, width: w, height: h }];
 
         updateSkinFile();
@@ -189,30 +198,31 @@
 
     function handleKeyPress(event: KeyboardEvent) {
         if (event.key == "Delete") {
-            console.log(targets);
-            targets.toReversed().forEach((target) => {
-                let regionIdData = target.getAttribute("data-region-id");
-                console.log(regionIdData);
-                if (!regionIdData) return;
-
-                let regionId = parseInt(regionIdData);
-                let region = $regions[regionId];
-
-                region?.free?.();
-
-                removeRegion(regionId);
-            });
-            
-            targets = [];
-
-            updateSkinFile();
+            handleDelete();
         }
     }
 
+    function handleDelete() {
+        targets.toReversed().forEach((target) => {
+            let regionIdData = target.getAttribute("data-region-id");
+            if (!regionIdData) return;
+
+            let regionId = parseInt(regionIdData);
+            let region = $regions[regionId];
+
+            region?.free?.();
+
+            removeRegion(regionId);
+        });
+
+        targets = [];
+
+        updateSkinFile();
+    }
 
     function saveImage() {
         if (!imgCanvas) return;
-        
+
         const src = imgCanvas.src;
         saveAs(src, "skin.png");
     }
@@ -245,23 +255,31 @@
           -->
         <div>
             <p>Instructions:</p>
-            <ul class="list-disc list-inside flex flex-col gap-1 pl-4">
+            <ul class="flex list-inside list-disc flex-col gap-1 pl-4">
                 <li>Drag and drop a skin file</li>
                 <li>
-                    <kbd>Double click</kbd> to add a region
+                    <kbd>Double click</kbd>
+                    to add a region
                 </li>
                 <li>
-                    <kbd>Click</kbd> to select a region
+                    <kbd>Click</kbd>
+                    to select a region
                 </li>
                 <li>
-                    <kbd>Shift + Click</kbd> to add a region to the selection
+                    <kbd>Shift + Click</kbd>
+                    to add a region to the selection
                 </li>
                 <li>
-                    <kbd>Delete</kbd> to delete the selected regions
+                    <kbd>Delete</kbd>
+                    to delete the selected regions
                 </li>
                 <li>
-                    <button on:click={saveImage}>Save image</button> or right click on the image and <kbd>Save image as...</kbd> to save the skin
+                    <button on:click={saveImage}>Save image</button>
+                    or right click on the image and
+                    <kbd>Save image as...</kbd>
+                    to save the skin
                 </li>
+            </ul>
         </div>
     </div>
     <div>
@@ -312,27 +330,41 @@
     />
 
     <Selecto
-        selectableTargets={[".region"]}
         hitRate={40}
         selectByClick={true}
         selectFromInside={false}
-        toggleContinueSelect={"shift"}
+        selectableTargets={[".region"]}
+        boundContainer={imgCanvas}
+        dragCondition={(e) => {
+            const withinX = e.inputEvent.clientX >= imgCanvasBounds.left && e.inputEvent.clientX <= imgCanvasBounds.right;
+            const withinY = e.inputEvent.clientY >= imgCanvasBounds.top && e.inputEvent.clientY <= imgCanvasBounds.bottom;
+
+            return withinX && withinY;
+        }}
+        on:dragEnd={({ detail: e }) => {
+            if (!e.isSelect) return;
+
+            let eventX = e.rect.left - imgCanvasBounds.left;
+            let eventY = e.rect.top - imgCanvasBounds.top;
+            let x = Math.round(eventX / getImagePixelWidth());
+            let y = Math.round(eventY / getImagePixelHeight());
+
+            let width = Math.round(e.rect.width / getImagePixelWidth());
+            let height = Math.round(e.rect.height / getImagePixelHeight());
+
+            if (width < 1 || height < 1) {
+                return;
+            }
+
+            addRegion(Math.max(0, x), Math.max(0, y), Math.max(1, width), Math.max(1, height));
+        }}
         on:selectEnd={({ detail: e }) => {
             if (e.isDragStart) {
                 e.inputEvent.preventDefault();
                 moveable.waitToChangeTarget().then(() => {
                     moveable.dragStart(e.inputEvent);
                 });
-            }
-            targets = e.selected;
-
-            if (targets.length == 0 && e.isDouble) {
-                let eventX = e.inputEvent.clientX - imgCanvasBounds.left;
-                let eventY = e.inputEvent.clientY - imgCanvasBounds.top;
-                let x = Math.round(eventX / getImagePixelWidth()) - 2;
-                let y = Math.round(eventY / getImagePixelHeight()) - 2;
-
-                addRegion(Math.max(0, x), Math.max(0, y), 4, 4);
+                targets = e.selected;
             }
         }}
     />
@@ -385,8 +417,8 @@
                 right top 15px;
         }
     }
-    
+
     kbd {
-        @apply bg-secondary-500/20 p-1 rounded;
+        @apply rounded bg-secondary-500/20 p-1;
     }
 </style>
