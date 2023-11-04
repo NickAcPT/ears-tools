e621000b80bc35fc9a35fdb43ab7a5c637038f4c
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-11-04 13:39:30 +0000

    (no description)

--- src/routes/tools/region-eraser/+page.ts
+++ src/routes/tools/region-eraser/+page.ts
@@ -2,12 +2,9 @@ import type { PageLoad } from './$types';
 
 export const load = (async () => {
     return {
-        /* 
-        title: "Blockbench Model Generator",
-        description: "To generate a Blockbench from a Minecraft skin, pick your settings, then drag your skin file to the upload region.",
-        ogdescription: "Tool to generate a Blockbench model from a Minecraft skin, including Ears mod features."*/
         title: "Ears Region Eraser",
         description: "To manage the ears erase regions, drag your skin file to the upload region.",
-        ogdescription: "Tool to edit Ears erase regions."
+        ogdescription: "Tool to edit Ears erase regions.",
+        noDescription: true,
     };
 }) satisfies PageLoad;
