e621000ceed92c865dea304ce8a830a264d5cd39
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-12-03 10:56:08 +0000

    Fix bbmodel description

--- src/routes/tools/bbmodel-generator/+page.ts
+++ src/routes/tools/bbmodel-generator/+page.ts
@@ -3,7 +3,7 @@ import type { PageLoad } from './$types';
 export const load = (async () => {
     return {
         title: "Blockbench Model Generator",
-        description: "To generate a Blockbench from a Minecraft skin, pick your settings, then drag your skin file to the upload region.",
+        description: "To generate a Blockbench model from a Minecraft skin, pick your settings, then drag your skin file to the upload region.",
         ogdescription: "Tool to generate a Blockbench model from a Minecraft skin, including Ears mod features."
     };
 }) satisfies PageLoad;
