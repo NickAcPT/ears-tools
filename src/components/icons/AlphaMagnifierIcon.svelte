e621000b6d128a9570685999787cbfb5fa634cd8
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-12-04 16:14:47 +0000

    Fix alfalfa icon

--- src/components/icons/AlphaMagnifierIcon.svelte
+++ src/components/icons/AlphaMagnifierIcon.svelte
@@ -9,14 +9,14 @@
             gradientUnits="userSpaceOnUse"
             gradientTransform="translate(-26.764 2.736)"
         >
-            <stop style="stop-color:#000;stop-opacity:1" offset="0" />
-            <stop style="stop-color:#000;stop-opacity:0" offset="1" />
+            <stop style="stop-color:currentColor;stop-opacity:1" offset="0" />
+            <stop style="stop-color:currentColor;stop-opacity:0" offset="1" />
         </linearGradient>
     </defs>
     <path
         fill="currentColor"
         d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h7c-.41-.25-.8-.56-1.14-.9-.33-.33-.61-.7-.86-1.1H6V4h7v5h5v1.18c.71.16 1.39.43 2 .82V8l-6-6m6.31 16.9c1.33-2.11.69-4.9-1.4-6.22-2.11-1.33-4.91-.68-6.22 1.4-1.34 2.11-.69 4.89 1.4 6.22 1.46.93 3.32.93 4.79.02L22 23.39 23.39 22l-3.08-3.1m-3.81.1a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z"
-        style="fill:#000"
+        style="fill:currentColor"
     />
     <path
         fill="currentColor"
