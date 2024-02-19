f03b71e8a56fd208f1bf07e5c18d0d640755f36b
NickAc <32451103+NickAcPT@users.noreply.github.com>
2024-02-19 12:17:24 +0000

    Read skin for SkinDropZone from the URL if it exists

--- src/components/SkinDropZone.svelte
+++ src/components/SkinDropZone.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
     import { writable, type Writable } from "svelte/store";
     import DropZone from "./DropZone.svelte";
-    import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher, onMount } from "svelte";
     import demoSkin from "$lib/assets/demo-skin.png";
+    import { page } from "$app/stores";
+    import { browser } from "$app/environment";
 
     let dispatch = createEventDispatcher();
 
     let pickerHovered = writable<boolean>(false);
     let filePicker: HTMLInputElement;
     export let slimArms: Writable<boolean> | undefined = undefined;
-        
+
     export let offerDemoSkin: boolean = true;
 
     function pickFile() {
@@ -25,21 +27,51 @@
 
     export async function selectDemoSkin() {
         if (!offerDemoSkin) return;
-        
+        if ($page.url.searchParams.has("base64")) return;
+
         let skin = await fetch(demoSkin);
         let data = await skin.arrayBuffer();
 
         let list = new DataTransfer();
         let file = new File([data], "demo-skin.png", { type: "image/png" });
         list.items.add(file);
-        
+
         if (slimArms) {
             $slimArms = true;
         }
         dispatch("files", list.files);
     }
-    
-    $: pickerBgClass = $pickerHovered ? 'bg-secondary-200' : 'bg-secondary-50';
+
+    $: pickerBgClass = $pickerHovered ? "bg-secondary-200" : "bg-secondary-50";
+
+    // Workaround: Load skin from page URL
+    onMount(async () => {
+        if (browser) {
+            const base64File = $page.url.searchParams.get("base64")?.replace(/-/g, "+")?.replace(/_/g, "/");
+            const slimArmsFromUrl = $page.url.searchParams.has("slim") !== null;
+            if (base64File) {
+                try {
+                    const data = atob(base64File);
+                    // Decode the string to a Uint8Array
+                    const uint8Array = new Uint8Array(data.length);
+                    for (let i = 0; i < data.length; i++) {
+                        uint8Array[i] = data.charCodeAt(i);
+                    }
+                    // "Upload" the file
+                    const list = new DataTransfer();
+                    const file = new File([uint8Array], "skin.png", { type: "image/png" });
+                    list.items.add(file);
+
+                    $slimArms = slimArmsFromUrl;
+
+                    dispatch("files", list.files);
+                } catch (e) {
+                    alert("Failed to load skin from URL. Are you sure that you encoded the skin correctly (base64url)?");
+                    console.error("Failed to load skin from URL", e);
+                }
+            }
+        }
+    });
 </script>
 
 <DropZone hovered={pickerHovered} on:files>
@@ -49,7 +81,7 @@
         <p>Drop your <slot name="file">skin</slot> file here or paste it in this page.</p>
         <hr class="m-0 w-full" />
         <div class="text-center">
-            <p class="inline">Or alternatively, </p>
+            <p class="inline">Or alternatively,</p>
             {#if offerDemoSkin}
                 <p class="inline">you can try a</p>
                 <button class="link" on:click={selectDemoSkin}>demo skin</button>
