e62100038ae0659e21d3e5881a043dc42b3b0e63
NickAc <32451103+NickAcPT@users.noreply.github.com>
2024-07-28 17:43:58 +0100

    OwO

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import { assets, base } from "$app/paths";
+    import { assets } from "$app/paths";
     import GitHubIcon from "./icons/GitHubIcon.svelte";
 </script>
 
 <nav class="sticky flex items-center justify-center flex-col left-0 top-0 border-b bg-background/60 border-text/20 backdrop-blur-2xl backdrop-saturate-150 h-[var(--navbar-height)]">
     <div class="flex w-full items-center justify-between max-w-[95%] lg:max-w-[85%]">
         <div>
-            <a class="flex max-w-fit items-center gap-2" href="{base}/">
+            <a class="flex max-w-fit items-center gap-2" href="/">
                 <img class="h-max" src="{assets}/favicon.png" alt="NickAc's Minecraft skin face" />
                 <h1 class="text-2xl">Ears Tools</h1>
             </a>
