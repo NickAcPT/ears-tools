e621000a81639f1fa433b13dc86be7e6c625cfe6
NickAc <32451103+NickAcPT@users.noreply.github.com>
2024-11-05 11:16:34 +0000

    Untrack init function
    Svelte did something similar internally for https://github.com/sveltejs/svelte/issues/13840

--- src/components/RequiresWasm.svelte
+++ src/components/RequiresWasm.svelte
@@ -2,7 +2,7 @@
 
 <script lang="ts">
     import RequiresJs from "./RequiresJs.svelte";
-    import type { Snippet } from "svelte";
+    import { untrack, type Snippet } from "svelte";
     
     interface RequiresWasmProps {
         init: () => Promise<void>;
@@ -14,13 +14,14 @@
 </script>
 
 <RequiresJs>
-    {#await updateReceiver != undefined && init()}
+    {#await updateReceiver != undefined && untrack(() => init())}
         <p class="text-center">Loading...</p>
     {:then}
         {#if children}
             {@render children()}
         {/if}
     {:catch error}
+        {@const _bruh = console.log(error)}
         <div class="relative left-0 my-5 flex w-full flex-col items-center gap-2 border-y-2 border-gray-400 bg-red-500/10 p-2">
             <p class="p-2 text-center text-xl">It seems like your browser doesn't support WebAssembly</p>
             <p>Please check if you have a recent version of your browser, and if you do, please contact @nickac on Discord.</p>
